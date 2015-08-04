50e824593df51cc47f33bd5464d3cb7369598d4a
wang-yichun <yishionwang314@163.com>
2015-08-04 16:37:03 +0800

    implementing DEFAULT @SettingScreen

--- Assets/ExampleProject/MainMenuSystem/Controllers/SettingsScreenController.cs
+++ Assets/ExampleProject/MainMenuSystem/Controllers/SettingsScreenController.cs
@@ -12,49 +12,52 @@ using UnityEngine;
 namespace uFrame.ExampleProject
 {
 
-    /*
+	/*
      * This example shows initializing of a viewmodel. Anytime you create a viewmodel using Contoller.Create{viewmodeltype}
      * instance goes through Initialize method, which you can use to your advantage.
      */
+	public class SettingsScreenController : SettingsScreenControllerBase
+	{
 
-    public class SettingsScreenController : SettingsScreenControllerBase
-    {
+		[Inject]
+		public SettingsService SettingsService;
 
-        [Inject] public SettingsService SettingsService;
+		public override void InitializeSettingsScreen (SettingsScreenViewModel viewModel)
+		{
+			base.InitializeSettingsScreen (viewModel);
 
-        public override void InitializeSettingsScreen(SettingsScreenViewModel viewModel)
-        {
-            base.InitializeSettingsScreen(viewModel);
+			/* Add known resolutions to the list */
+			viewModel.AvailableResolutions.AddRange (SettingsService.AvailableResolutions);
 
-            /* Add known resolutions to the list */
-            viewModel.AvailableResolutions.AddRange(SettingsService.AvailableResolutions);
+			/* Setup current resolution */
+			viewModel.Resolution = SettingsService.Resolution;
 
-            /* Setup current resolution */
-            viewModel.Resolution = SettingsService.Resolution;
+			/* Setup volume */
+			viewModel.Volume = SettingsService.Volume;
+		}
 
-            /* Setup volume */
-            viewModel.Volume = SettingsService.Volume;
-        }
+		public override void Apply (SettingsScreenViewModel viewModel)
+		{
+			base.Apply (viewModel);
 
-        public override void Apply(SettingsScreenViewModel viewModel)
-        {
-            base.Apply(viewModel);
-
-            /* Pass selected values to the service */
-            SettingsService.Resolution = viewModel.Resolution;
-            SettingsService.Volume = viewModel.Volume;
-        }
+			/* Pass selected values to the service */
+			SettingsService.Resolution = viewModel.Resolution;
+			SettingsService.Volume = viewModel.Volume;
+		}
 
 
-        /* 
+		/* 
          * Home Work: implement this command so, that settings are reverted to default values.
          * Hint: Declare default settings in the SettingsService.
          * Don't forget to update both SettingsService and SettingsScreen
          */
 
-        public override void Default(SettingsScreenViewModel viewModel)
-        {
-            base.Default(viewModel);
-        }
-    }
+		public override void Default (SettingsScreenViewModel viewModel)
+		{
+			base.Default (viewModel);
+
+			viewModel.Volume = SettingsService.Volume = SettingsService.DefaultVolume;
+			viewModel.Resolution = SettingsService.Resolution = SettingsService.DefaultResolution;
+		}
+	}
 }
